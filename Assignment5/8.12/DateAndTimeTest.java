f67b34f6261cc3f80396832e6bf423d33eb8ac55
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-04-18 03:13:28 +0200

    Delete Assignment5 directory

--- Assignment5/8.12/DateAndTimeTest.java
+++
@@ -1,8 +0,0 @@
-public class DateAndTimeTest {
-    public static void main(String[] args) {
-        DateAndTime dateTime = new DateAndTime(2, 28, 2023, 23, 59, 59);
-        System.out.println(dateTime.toUniversalString()); // prints "2/28/2023 23:59:59"
-        dateTime.incrementHour();
-        System.out.println(dateTime.toUniversalString()); // prints "3/1/2023 00:59:59"
-    }
-}
